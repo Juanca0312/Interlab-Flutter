8fb9d346ca24a044ab5cf32a31b7372034526825
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-05-22 11:08:52 -0500

    Added color and gradient library.
    Use IColors and IGradient to use the assets.

---
+++ assets/interlab_gradients.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+import 'interlab_colors.dart';
+
+class IGradients{
+  static const LinearGradient green_lightblue = LinearGradient(
+      begin: Alignment.topRight,
+      end: Alignment.bottomLeft,
+      colors: [IColors.green, IColors.lightblue]);
+  static const LinearGradient yellow_orange = LinearGradient(
+      begin: Alignment.topRight,
+      end: Alignment.bottomLeft,
+      colors: [IColors.yellow, IColors.orange]);
+  static const LinearGradient purple_pink = LinearGradient(
+      begin: Alignment.topRight,
+      end: Alignment.bottomLeft,
+      colors: [IColors.purple, IColors.pink]);
+  static const LinearGradient lightblue_blue = LinearGradient(
+      begin: Alignment.topRight,
+      end: Alignment.bottomLeft,
+      colors: [IColors.lightblue, IColors.blue]);
+}
