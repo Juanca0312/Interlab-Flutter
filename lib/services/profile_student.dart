9e40acf1b39dabf94bb15295e1f7bc9382c747ca
Juan Carlos Hernández <jchernandez0312@gmail.com>
2021-05-22 12:01:35 -0500

    :sparkles: Finished Profile Get
    :sparkles: Finished Profile Get with api

--- lib/services/profile_student.dart
+++ lib/services/profile_student.dart
@@ -21,7 +21,7 @@ class ProfileS{
   Future<void> getData() async {
     try{
       Response response = await get(
-        Uri.parse('https://interlabapi.herokuapp.com/api/users/1'),
+        Uri.parse('https://interlabapi.herokuapp.com/api/profiles/1'),
         // Send authorization headers to the backend.
         headers: {
           HttpHeaders.authorizationHeader: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwaXRpIn0.Zq4fRNnpFFzaC0nuNopJuU3EHciKTk4H2XsQU8wY6wZVqnw_Xdfl4sDjjSks4lAarh1mf06bwS8wOb06LzFGuw',
@@ -30,8 +30,17 @@ class ProfileS{
 
       Map data = jsonDecode(response.body);
 
-      firstName = data['username'];
-      print(firstName);
+      firstName = data['firstName'];
+      role = data['role'];
+      lastName = data['lastName'];
+      description = data['description'];
+      email = data['email'];
+      university = data['university'];
+      phone = data['phone'];
+      semester = data['semester'].toString();
+      degree = data['degree'];
+      //print(data['university']);
+
     }
     catch(e){
       print('caught error $e');
