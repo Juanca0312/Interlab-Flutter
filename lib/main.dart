827e3bf747457dd8d689c233e73a0ef9d2fe743d
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-05-23 11:40:46 -0500

    ✨Login Screen
    Created Login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:interlab/pages/login.dart';
 import 'package:interlab/pages/student_dashboard.dart';
 
 void main() {
   runApp(MaterialApp(
-    theme: ThemeData(
-        fontFamily: 'Poppins'
-    ),
+    theme: ThemeData(fontFamily: 'Poppins'),
     initialRoute: '/',
     routes: {
       '/': (context) => StudentDashboard(),
       '/studentDashboard': (context) => StudentDashboard(),
+      '/login': (context) => LoginPage(),
     },
   ));
 }
