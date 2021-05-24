d3d114cf2683cbd21e7af1e6e36e8fd56055f5db
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-05-24 00:05:45 -0500

    Finished service for making API calls for student_home.

---
+++ lib/services/application_service.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart';
+import 'package:interlab/models/application.dart';
+
+class ApplicationService{
+  List<Application> applicationList=[];
+
+  Future<void> getData() async {
+    try {
+      Response response = await get(
+        Uri.parse('https://interlabapi.herokuapp.com/api/users/1/internships'),
+        headers: {
+          HttpHeaders.authorizationHeader: 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwaXRpIn0.Zq4fRNnpFFzaC0nuNopJuU3EHciKTk4H2XsQU8wY6wZVqnw_Xdfl4sDjjSks4lAarh1mf06bwS8wOb06LzFGuw',
+        },
+      );
+      Map data = jsonDecode(response.body);
+      for(var i = 0; i<data['numberOfElements']; i++){
+        applicationList.add(
+            new Application(
+                data['content'][i]['jobTitle'],
+                'MockCompany',
+                data['content'][i]['location'],
+                data['content'][i]['state'])
+        );
+      }
+    }
+    catch(e){
+      print('caught error $e');
+    }
+  }
+}
