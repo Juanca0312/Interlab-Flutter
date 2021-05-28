8bcf49d83f418ad660ea013d306387d20b9b5323
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-05-28 18:28:43 -0500

    Refactored container padding and indentation.

--- lib/widgets/student_home_empty.dart
+++ lib/widgets/student_home_empty.dart
@@ -17,57 +17,58 @@ class _StudentHomeEmptyState extends State<StudentHomeEmpty> {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
       child: Container(
+        padding: EdgeInsets.all(8.0),
         child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Expanded(flex: 15, child: Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 5, 0, 10),
-                    child: UnDraw(illustration: UnDrawIllustration.adventure_map, color: IColors.lightblue),
-                  )),
-                  Expanded(
-                      flex: 4,
-                      child: Center(
-                          child: Text(
-                            'No hay aplicaciones\nrecientes!',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 20
-                            ),
-                          )
-                      )
-                  ),
-                  Expanded(
-                      flex: 5,
-                      child: Center(
-                          child: Text(
-                            'Parece que no tienes aplicaciones\na pasantías, encuentra tu pasantía\ndeseada ahora.',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.grey[700],
-                                fontWeight: FontWeight.w300,
-                                fontSize: 13
-                            ),
-                          )
-                      )
-                  ),
-                ],
+          child: Column(
+            children: [
+              Expanded(
+                flex: 15,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 10),
+                  child: UnDraw(illustration: UnDrawIllustration.adventure_map, color: IColors.lightblue),
+                )
               ),
-            )
+              Expanded(
+                flex: 4,
+                child: Center(
+                  child: Text(
+                    'No hay aplicaciones\nrecientes!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20
+                    ),
+                  )
+                )
+              ),
+              Expanded(
+                flex: 5,
+                child: Center(
+                  child: Text(
+                    'Parece que no tienes aplicaciones\na pasantías, encuentra tu pasantía\ndeseada ahora.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontWeight: FontWeight.w300,
+                      fontSize: 13
+                    ),
+                  )
+                )
+              ),
+            ],
+          )
         ),
         decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(15)),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(.3),
-                spreadRadius: 3,
-                blurRadius: 8,
-                offset: Offset(5, 5),
-              ),
-            ]
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(15)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(.3),
+              spreadRadius: 3,
+              blurRadius: 8,
+              offset: Offset(5, 5),
+            ),
+          ]
         ),
       ),
     );
