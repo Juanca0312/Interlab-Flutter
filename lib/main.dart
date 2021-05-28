cef527ecc5c0ad119e8118985b1d621244fe5da7
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-05-28 18:01:07 -0500

    ✨Implemented Token
    Implemented token retrieve from user credentials.

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
     routes: {
       '/': (context) => StudentDashboard(),
       '/studentDashboard': (context) => StudentDashboard(),
-      '/login': (context) => LoginPage(),
+      '/login': (context) => Login(),
       '/registerStudent': (context) => RegisterStudent(),
       '/registerCompany': (context) => RegisterCompany(),
     },
