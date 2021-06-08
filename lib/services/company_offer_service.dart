41c81a5076b6070f3d0e6a9afa59ec8c2cedbf30
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-06-08 13:19:21 -0500

    Created company_offer_service and successfully made post requests.

---
+++ lib/services/company_offer_service.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:interlab/models/company_offer.dart';
+
+class CompanyOfferService{
+  CompanyOffer companyOffer;
+
+  Future<void> createData() async {
+    final body = {
+      'state': companyOffer.status,
+      'description': companyOffer.description,
+      'startingDate': companyOffer.startingDate,
+      'finishingDate': companyOffer.finishingDate,
+      'salary': companyOffer.salary,
+      'location': companyOffer.location,
+      'jobTitle': companyOffer.title
+    };
+    try {
+      Response response = await post(
+        Uri.parse('https://interlabapi.herokuapp.com/api/companies/1/internships'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwaXRpIn0.Zq4fRNnpFFzaC0nuNopJuU3EHciKTk4H2XsQU8wY6wZVqnw_Xdfl4sDjjSks4lAarh1mf06bwS8wOb06LzFGuw'
+        },
+        body: jsonEncode(body),
+      );
+      print(response.statusCode);
+    }
+    catch(e){
+      print('caught error $e');
+    }
+  }
+}
