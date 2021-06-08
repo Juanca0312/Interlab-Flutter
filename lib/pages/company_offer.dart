b1daf021f9577cc112ee085139df563d79b89a3f
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-06-07 21:12:37 -0500

    Added scrollbar to indicate that there is more content below.

--- lib/pages/company_offer.dart
+++ lib/pages/company_offer.dart
@@ -28,90 +28,95 @@ class _OfferState extends State<Offer> {
       padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
       child: Container(
           child: ClipRRect(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
-              child: MediaQuery.removePadding(
-                removeTop: true,
-                context: context,
+            child: MediaQuery.removePadding(
+              removeTop: true,
+              context: context,
+              child: RawScrollbar(
+                isAlwaysShown: true,
+                radius: Radius.circular(10),
+                thumbColor: IColors.blue,
                 child: ListView(children: [
                   Form(
                     key: _formKey,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        ITextFormInput(
-                          textController: titleC,
-                          label: 'Título de la oferta',
-                          hint: 'Ex. UI Design Intern',
-                        ),
-                        ITextFormInput(
-                          textController: descriptionC,
-                          label: 'Descripción de la oferta',
-                          hint: '',
-                        ),
-                        ITextFormInput(
-                          textController: startingDateC,
-                          label: 'Fecha de inicio',
-                          hint: 'dd/mm/aaaa',
-                        ),
-                        ITextFormInput(
-                          textController: finishingDateC,
-                          label: 'Fecha de fin',
-                          hint: 'dd/mm/aaaa',
-                        ),
-                        ITextFormInput(
-                          textController: locationC,
-                          label: 'Ubicación del trabajo',
-                          hint: 'Ex. Surco - Lima',
-                        ),
-                        ITextFormInput(
-                          textController: salaryC,
-                          label: 'Salario mensual',
-                          hint: 'Ex. S/ 3000',
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        InkWell(
-                          onTap: () {
-                            print(titleC.text);
-                          },
-                          child: Center(
-                            child: Container(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 60, vertical: 0),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Icon(
-                                    Icons.check_rounded,
-                                    color: Colors.white,
-                                  ),
-                                  Text(
-                                    'Crear oferta',
-                                    style: TextStyle(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          ITextFormInput(
+                            textController: titleC,
+                            label: 'Título de la oferta',
+                            hint: 'Ex. UI Design Intern',
+                          ),
+                          ITextFormInput(
+                            textController: descriptionC,
+                            label: 'Descripción de la oferta',
+                            hint: '',
+                          ),
+                          ITextFormInput(
+                            textController: startingDateC,
+                            label: 'Fecha de inicio',
+                            hint: 'dd/mm/aaaa',
+                          ),
+                          ITextFormInput(
+                            textController: finishingDateC,
+                            label: 'Fecha de fin',
+                            hint: 'dd/mm/aaaa',
+                          ),
+                          ITextFormInput(
+                            textController: locationC,
+                            label: 'Ubicación del trabajo',
+                            hint: 'Ex. Surco - Lima',
+                          ),
+                          ITextFormInput(
+                            textController: salaryC,
+                            label: 'Salario mensual',
+                            hint: 'Ex. S/ 3000',
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          InkWell(
+                            onTap: () {
+                              print(titleC.text);
+                            },
+                            child: Center(
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 60, vertical: 0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Icon(
+                                      Icons.check_rounded,
                                       color: Colors.white,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w500,
                                     ),
-                                  ),
-                                ],
-                              ),
-                              height: 50,
-                              width: 350,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(40),
-                                gradient: IGradients.purple_pink,
+                                    Text(
+                                      'Crear oferta',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                height: 50,
+                                width: 350,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(40),
+                                  gradient: IGradients.purple_pink,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                      ],
+                          SizedBox(
+                            height: 30,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ]),
